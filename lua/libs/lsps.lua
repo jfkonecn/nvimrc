e315059a1cf23bc723d1bd4ace3a125b1f94f32e
John <jfkonecn@gmail.com>
2022-11-06 12:11:08 -0500

    a

--- lua/libs/lsps.lua
+++ lua/libs/lsps.lua
@@ -197,3 +197,12 @@ cmp.setup {
 require'lspconfig'.tailwindcss.setup{}
 
 
+require'lspconfig'.rls.setup {
+  settings = {
+    rust = {
+      unstable_features = true,
+      build_on_save = false,
+      all_features = true,
+    },
+  },
+}
