bea27e919176c449a660c431a6acb0453b458ba1
John Konecny <jfkonecn@gmail.com>
2023-03-18 10:45:12 -0400

    a

--- lua/libs/remaps.lua
+++ lua/libs/remaps.lua
@@ -5,36 +5,25 @@
 --      ^ map
 
 local function map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
-
 
 vim.g.mapleader = " "
 
 -- do grep telescope search
 map("n", "<leader>ps", ":lua require('telescope.builtin').grep_string({ search = vim.fn.input(\"Grep For > \")})<CR>")
 
+map("n", "<leader>fa", ":lua require('telescope.builtin').find_files({ hidden = true})<CR>")
+
 -- hide search results
 map("n", "<leader>hs", ":noh<CR>")
 
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
-
-local telescope = require('telescope')
-
-telescope.setup {
-    pickers = {
-        find_files = {
-            hidden = true
-        }
-    }
-}
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
