54f01abc7a576e8bbee2145b2ec9fd4decb5ede6
John Konecny <jfkonecn@gmail.com>
2023-03-19 12:24:55 -0400

    a

--- lua/libs/plugins.lua
+++ lua/libs/plugins.lua
@@ -5,52 +5,50 @@
 -- lua syntax
 -- https://dev.to/vonheikemen/neovim-using-vim-plug-in-lua-3oom
 -- https://github.com/junegunn/vim-plug
-local Plug = vim.fn['plug#']
-vim.call('plug#begin')
+local Plug = vim.fn["plug#"]
+vim.call("plug#begin")
 
 -- language server
 -- https://github.com/neovim/nvim-lspconfig
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-Plug('neovim/nvim-lspconfig')
-Plug('hrsh7th/nvim-cmp') -- Autocompletion plugin
-Plug('hrsh7th/cmp-nvim-lsp') -- LSP source for nvim-cmp
-Plug('saadparwaiz1/cmp_luasnip') -- Snippets source for nvim-cmp
-Plug('L3MON4D3/LuaSnip') -- Snippets plugin
-Plug('hrsh7th/cmp-nvim-lsp-signature-help')
+Plug("neovim/nvim-lspconfig")
+Plug("hrsh7th/nvim-cmp") -- Autocompletion plugin
+Plug("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
+Plug("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+Plug("L3MON4D3/LuaSnip") -- Snippets plugin
+Plug("hrsh7th/cmp-nvim-lsp-signature-help")
 
 -- fuzzy finder
 -- https://github.com/nvim-telescope/telescope.nvim
-Plug('nvim-lua/plenary.nvim')
-Plug('nvim-telescope/telescope.nvim', { tag = '0.1.0' })
+Plug("nvim-lua/plenary.nvim")
+Plug("nvim-telescope/telescope.nvim", { tag = "0.1.0" })
 
 -- lsp manager
-Plug('williamboman/mason.nvim')
-
+Plug("williamboman/mason.nvim")
 
 -- theme
-Plug('gruvbox-community/gruvbox')
+Plug("gruvbox-community/gruvbox")
 
 -- is a parser generator tool and an incremental parsing library
-Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate'})
+Plug("nvim-treesitter/nvim-treesitter", { ["do"] = ":TSUpdate" })
 
 -- git manager
-Plug('tpope/vim-fugitive')
+Plug("tpope/vim-fugitive")
 
 -- look into
 -- Plug('mbbill/undotree')
 
 -- spell checker
-Plug('kamykn/spelunker.vim')
+Plug("kamykn/spelunker.vim")
 
 -- lint manager
 -- Plug('mfussenegger/nvim-lint')
-Plug('jose-elias-alvarez/null-ls.nvim')
+Plug("jose-elias-alvarez/null-ls.nvim")
 
 -- comment out lines
-Plug('preservim/nerdcommenter')
+Plug("preservim/nerdcommenter")
 
 -- don't need on 0.9.0 and beyond
-Plug('MunifTanjim/exrc.nvim')
+Plug("MunifTanjim/exrc.nvim")
 
-
-vim.call('plug#end')
+vim.call("plug#end")
